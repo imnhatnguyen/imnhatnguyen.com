81e863f855ebbcb652f9cf00780016d17dbb0b33
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-07-25 21:19:36 +0700

    fix: add actual width and height of photos

--- src/components/images/imageList.tsx
+++ src/components/images/imageList.tsx
@@ -5,8 +5,8 @@ function createCaption(title: string, date: string, description: string) {
 const imageList = [
   {
     id: 'AMWts8COqYpDshFIoEO7EUEwZcEFGMDnPhJpg0IkcDDJYOdX722mxp0Ko9zHjpyO9jRRsYaJSdMU9dav3AM3xRVwLE1j5lvmQ2h1dK7OL8horxreqIvXSqaqj0HsjbTcY6yqCr7W6ep5koqLAiPMopLGmvI',
-    width: 6,
-    height: 4,
+    width: 6016,
+    height: 4000,
     caption: createCaption(
       'Me hiding',
       'Feb 5, 2016',
@@ -15,8 +15,8 @@ const imageList = [
   },
   {
     id: 'AMWts8Cxu6g4VBAspWay3gMOhivKVMt0kWsA8bDR9waq1v24d80ej031owQZuhce3D_ZpgU_sdWPYZbrsTWSflKcwcht4FGB8W3dVElq8yLD7kKxAAYLoPFlBZ6D7PuobumS2Uey9wx5pNUI4mo8myULIYI',
-    width: 4,
-    height: 6,
+    width: 4001,
+    height: 6018,
     caption: createCaption(
       'Into the wood',
       'Jul 6, 2016',
@@ -25,8 +25,8 @@ const imageList = [
   },
   {
     id: 'AMWts8BqTbstFhzBsopeT7xiyGWiQaoPFjsVKIkmNm18GwFnqKPxUeKdcWAQplOcytjsjhmTuYbU4B150AN5G24tNbA8TwwXk4_M3Kd-66h4qbSVH80Wkw139vJ3zXcoGo_bTzj1HMNQ5jLuxb1ddIYkNZA',
-    width: 7,
-    height: 5,
+    width: 3647,
+    height: 2547,
     caption: createCaption(
       'Sunset',
       'Jan 1, 2015',
@@ -35,8 +35,8 @@ const imageList = [
   },
   {
     id: 'AMWts8DKs507QnJGiMRiEZ5Pis1HeK27V72Z81QCIQ97PdJgJoA4COmB_1NL-xzp2j2RuMP-NyzyL5w86HGbywcJD26mjE9IrD_D1tFeuApFsAysCds5M-ZpO1roe8qvLFrUbDPc8oREcevLYN61JvNOXf4',
-    width: 6,
-    height: 4,
+    width: 6016,
+    height: 4000,
     caption: createCaption(
       'Ha Noi dusk',
       'Sep 2, 2021',
@@ -45,8 +45,8 @@ const imageList = [
   },
   {
     id: 'AMWts8CfCPRUY9RWr2o3RkBeHoffUOsUDK9oWsze3aZVvo-v3IOp53AabdZa4a5Z8oU25h50JkCrS5ByaT16VyPYl02-JqYVVOjuKNW_tsJIFdXKhjSN8Bw3Kmtgp_TQssrC-GxjkibB6JK-HyGsnsMRlLU',
-    width: 6,
-    height: 4,
+    width: 6016,
+    height: 4000,
     caption: createCaption(
       'Eggs',
       'Jan 19, 2016',
@@ -55,8 +55,8 @@ const imageList = [
   },
   {
     id: 'AMWts8CBJ3Q9guajOqVU_M9HXWSh7Hqr5h0LnN2RO52vpKLQy0jfb-QykhKHMNdhSXVqk7v7tDORgPgjsOMTcmlrCY5Pci8XIfTUpDxy0cpjZurLXUC1Ye7pRRpZ6bp60G2h07G2cVK8osPYJHGbQ8j0mxc',
-    width: 14,
-    height: 19,
+    width: 1417,
+    height: 1889,
     caption: createCaption(
       'Unknown flowers',
       'Nov 5, 2014',
@@ -65,8 +65,8 @@ const imageList = [
   },
   {
     id: 'AMWts8CNgnQI6vMX_GDO6YOAnWV-W700mB4kfdczrnr0Mj09UffAwiXKvFpkGMnu2saCmRH-cItu2ITydFepYHLDELO83JtmPYfL1sU69ZEVn9-UYfcxR4y_zB1cFaHf5XoHUeDEZen88XrD2TjeG1-XMw0',
-    width: 17,
-    height: 6,
+    width: 3404,
+    height: 1260,
     caption: createCaption(
       'AMO 2017 Facebook cover',
       'May 7, 2017',
@@ -75,8 +75,8 @@ const imageList = [
   },
   {
     id: 'AMWts8DlvgIFHvBb5K4-QBbvB-1bBGArqk2Fyq-8Xxc7zGUP39ZYjMDr-rghxZUmPPuUob2qVfBgcj2d_mGcuhxSEjGWnkygNSvhZQl8fsGweFh_IpVS0SIX7fsQizqSBcpxaUrP4F4Qqi_zkAH3TSHrAX8',
-    width: 8,
-    height: 11,
+    width: 3231,
+    height: 4411,
     caption: createCaption(
       '<a href="https://henibeauty.com" target="_blank">HeNi Beauty Salon</a>\'s nail design',
       'Nov 20, 2022',
@@ -85,8 +85,8 @@ const imageList = [
   },
   {
     id: 'AMWts8D2WrblT0QWdQfhEyP0QblBGV-gqTI88Z_Sgb4g3Zr9gCRz5L2rNvibx00CwAW0GfEGrnMpTeab06qnHLXXbJF-wbw027yrp-tKl6hGP61L8J3dwa4TSKYoRbjIWyg5kkU3SXus3orRq_ldgnXs82Q',
-    width: 2,
-    height: 1,
+    width: 1400,
+    height: 704,
     caption: createCaption(
       'UX Design word cloud',
       'Dec 2, 2022',
@@ -95,8 +95,8 @@ const imageList = [
   },
   {
     id: 'AMWts8DyznCXJOX-L9hddGwzY4ZIMGFS3dlKwXFeYDq_GYO6ZQ1NTKyXnHOV88xxGGmqBl9EmFtEzJQbYprPJpui2-GoNMM9F2t8FifhUDRzYBWg7G3oV4jd25wRKEa8rKGrgBk2G-y9-1kvDLRIo6MxLvc',
-    width: 3,
-    height: 2,
+    width: 3984,
+    height: 2592,
     caption: createCaption(
       'Pink daisies',
       'Jan 6, 2017',
@@ -105,8 +105,8 @@ const imageList = [
   },
   {
     id: 'AMWts8BysR17FwsM03INIHRwmJFG4WRcCyGjNzEtGLlvcWQsbIUSLSWWei3s89xmr8_AdG8AVDqKWGC3H-ilRSulirj3vQB7Unq0eL3R-q7xMBib8X8mL8mdZI0WY7GmorgtMG6eB_gy01eeW0zQQ1o-uFw',
-    width: 7,
-    height: 11,
+    width: 3674,
+    height: 5526,
     caption: createCaption(
       'PuriTea cleanser',
       'Sep 2, 2021',
