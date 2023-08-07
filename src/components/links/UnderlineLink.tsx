10b5bd2f2d73c37059963864a056a22520aac25b
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-08-07 21:39:31 +0700

    fix: change link style

--- src/components/links/UnderlineLink.tsx
+++ src/components/links/UnderlineLink.tsx
@@ -6,7 +6,7 @@ import UnstyledLink, {
 } from '@/components/links/UnstyledLink';
 
 export const UnderlineLinkClassName =
-  'focus-visible-only inline-flex items-center border-b border-dotted border-gray-800 hover:border-solid hover:border-primary-500 dark:border-gray-200 dark:hover:border-primary-400 md:border-b-2';
+  'focus-visible-only inline-flex items-center underline';
 
 const UnderlineLink = forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
   ({ children, className, ...rest }, ref) => {
