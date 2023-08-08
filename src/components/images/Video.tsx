d622fde1665ea7708ef7b1d3bf18a94b6014a843
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-08-08 10:55:59 +0700

    feat: add track element

--- src/components/images/Video.tsx
+++ src/components/images/Video.tsx
@@ -7,7 +7,15 @@ type VideoType = {
 export default function Video({ src, width, alt }: VideoType) {
   return (
     <figure>
-      <video controls src={src} width={width} className='mx-auto rounded-2xl' />
+      <video controls width={width} className='mx-auto rounded-2xl'>
+        <source src={src} type='video/webm' />
+        <track
+          src='https://drive.google.com/uc?id=14fkK6oZRveeueyAS55XkEhTMgOCmDgiD'
+          kind='captions'
+          lang='en'
+          label='English'
+        />
+      </video>
       <figcaption className='-mt-5 text-center'>{alt}</figcaption>
     </figure>
   );
