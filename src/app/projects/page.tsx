a51bb08dc760da4af783feac20515db225a5fee0
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-08-12 19:01:13 +0700

    fix: redesign project card

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,14 @@
 import { allProjects } from 'contentlayer/generated';
-import { compareDesc } from 'date-fns';
+import { compareDesc, format } from 'date-fns';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import Balancer from 'react-wrap-balancer';
+
+import CloudImage from '@/components/images/CloudImage';
 
 export const metadata: Metadata = {
   title: 'Project',
-  description: 'Read my thoughts on software development, design, and more.',
+  description: 'Showcase of projects that I built',
 };
 
 export default async function ProjectPage() {
@@ -14,23 +17,30 @@ export default async function ProjectPage() {
   );
 
   return (
-    <section>
-      <h1 className='font-bold text-2xl mb-8 tracking-tighter'>
-        see my projects
-      </h1>
-      {posts.map((post) => (
-        <Link
-          key={post.slug}
-          className='flex flex-col space-y-1 mb-4'
-          href={`/projects/${post.slug}`}
-        >
-          <div className='w-full flex flex-col'>
-            <p className='text-neutral-900 dark:text-neutral-100 tracking-tight'>
-              {post.title}
-            </p>
-          </div>
-        </Link>
-      ))}
-    </section>
+    <>
+      <h1 className='mb-7'>Projects</h1>
+      <div className='grid gap-6 sm:grid-cols-2 sm:gap-y-14 xl:grid-cols-3'>
+        {posts.map((post) => (
+          <Link key={post.slug} href={`/projects/${post.slug}`}>
+            <CloudImage
+              className='pointer-events-none'
+              id={post.image}
+              alt={post.title}
+              width={550}
+              height={(550 * 1) / 2}
+              hasCaption={false}
+            />
+            <div className='px-2 py-3 space-y-1'>
+              <Balancer className='text-base font-semibold'>
+                {post.title}
+              </Balancer>
+              <p className='text-gray-500 dark:text-gray-400'>
+                {format(new Date(post.publishedAt), 'MMM dd, yyyy')}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
