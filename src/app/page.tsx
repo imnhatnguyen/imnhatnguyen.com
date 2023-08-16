1491747a6a29e57cc2f339da51c01a173c6d956a
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-08-16 15:04:04 +0700

    feat: add projectcard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { allProjects, Project } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
 import type { Metadata } from 'next';
+import { HiOutlineArrowRight } from 'react-icons/hi';
 
+import ProjectCard from '@/components/layouts/ProjectCard';
 import ButtonLink from '@/components/links/ButtonLink';
 export const metadata: Metadata = {
   title: 'Home',
@@ -7,6 +11,11 @@ export const metadata: Metadata = {
 };
 
 export default function HomePage() {
+  const projects = allProjects
+    .slice(0, 3)
+    .sort((a: Project, b: Project) =>
+      compareDesc(new Date(a.publishedAt), new Date(b.publishedAt)),
+    );
   return (
     <>
       <section className='mt-32 flex flex-col'>
@@ -26,6 +35,22 @@ export default function HomePage() {
             About me
           </ButtonLink>
         </div>
+      </section>
+      <section className='mt-36'>
+        <h2 className='text-2xl md:text-3xl'>Featured projects</h2>
+        <div className='mt-5 grid gap-6 sm:grid-cols-2 sm:gap-y-14 xl:grid-cols-3'>
+          {projects.map((post: Project) => (
+            <ProjectCard key={post.slug} post={post} />
+          ))}
+        </div>
+        <ButtonLink
+          href='/projects'
+          type='text'
+          rightIcon={HiOutlineArrowRight}
+          className='mt-2'
+        >
+          See more projects
+        </ButtonLink>
       </section>
     </>
   );
