1bc12c784ae66ac55b79320f1971a00a0aecec1e
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2023-08-19 13:55:36 +0700

    refactor: remove UnstyledLink module

--- src/components/links/ButtonLink.tsx
+++ src/components/links/ButtonLink.tsx
@@ -1,11 +1,7 @@
-import { forwardRef } from 'react';
+import Link from 'next/link';
 import { IconType } from 'react-icons';
 
 import clsxMerge from '@/lib/clsxMerge';
-
-import UnstyledLink, {
-  UnstyledLinkProps,
-} from '@/components/links/UnstyledLink';
 
 enum ButtonLinkType {
   'filled',
@@ -17,83 +13,80 @@ enum ButtonLinkSize {
   'base',
 }
 
-export type ButtonLinkProps = {
+type ButtonLinkProps = {
+  className?: string;
+  href: string;
   type?: keyof typeof ButtonLinkType;
   size?: keyof typeof ButtonLinkSize;
   leftIcon?: IconType;
   rightIcon?: IconType;
+  children: React.ReactNode;
   leftIconClassName?: string;
   rightIconClassName?: string;
-} & UnstyledLinkProps;
+};
 
-const ButtonLink = forwardRef<HTMLAnchorElement, ButtonLinkProps>(
-  (
-    {
-      children,
-      className,
-      type = 'filled',
-      size = 'base',
-      leftIcon: LeftIcon,
-      rightIcon: RightIcon,
-      leftIconClassName,
-      rightIconClassName,
-      ...rest
-    },
-    ref,
-  ) => {
-    return (
-      <UnstyledLink
-        ref={ref}
-        {...rest}
-        className={clsxMerge(
-          'inline-flex items-center rounded-full font-medium',
-          'px-3 md:px-4 py-2',
-          //#region  //*=========== Types ===========
-          [
-            type === 'filled' && [
-              'bg-primary-700 hover:bg-primary-800 text-white ',
-              'dark:bg-primary-400 dark:text-dark dark:hover:bg-primary-500',
-              'disabled:bg-primary-700',
-            ],
-            type === 'outlined' && [
-              'text-primary-700 dark:text-primary-400',
-              'outline outline-1 outline-gray-200  dark:outline-gray-700  md:outline-2',
-              'hover:bg-primary-50 dark:hover:bg-primary-400/10',
-            ],
-            type === 'text' && [
-              'text-primary-700 dark:text-primary-400',
-              'hover:bg-primary-50 dark:hover:bg-primary-400/10',
-            ],
+const ButtonLink = ({
+  children,
+  className,
+  type = 'filled',
+  size = 'base',
+  leftIcon: LeftIcon,
+  rightIcon: RightIcon,
+  leftIconClassName,
+  rightIconClassName,
+  ...rest
+}: ButtonLinkProps) => {
+  return (
+    <Link
+      {...rest}
+      className={clsxMerge(
+        'inline-flex items-center rounded-full font-medium',
+        'px-3 md:px-4 py-2',
+        //#region  //*=========== Types ===========
+        [
+          type === 'filled' && [
+            'bg-primary-700 hover:bg-primary-800 text-white ',
+            'dark:bg-primary-400 dark:text-dark dark:hover:bg-primary-500',
+            'disabled:bg-primary-700',
           ],
-          //#endregion  //*======== Types ===========
-          'disabled:cursor-not-allowed',
-          className,
-        )}
-      >
-        {LeftIcon && (
-          <div className={clsxMerge([size === 'base' && 'mr-2'])}>
-            <LeftIcon
-              className={clsxMerge(
-                [size === 'base' && 'h-5 w-5'],
-                leftIconClassName,
-              )}
-            />
-          </div>
-        )}
-        {children}
-        {RightIcon && (
-          <div className={clsxMerge([size === 'base' && 'ml-2'])}>
-            <RightIcon
-              className={clsxMerge(
-                [size === 'base' && 'h-5 w-5'],
-                rightIconClassName,
-              )}
-            />
-          </div>
-        )}
-      </UnstyledLink>
-    );
-  },
-);
+          type === 'outlined' && [
+            'text-primary-700 dark:text-primary-400',
+            'outline outline-1 outline-gray-200  dark:outline-gray-700  md:outline-2',
+            'hover:bg-primary-50 dark:hover:bg-primary-400/10',
+          ],
+          type === 'text' && [
+            'text-primary-700 dark:text-primary-400',
+            'hover:bg-primary-50 dark:hover:bg-primary-400/10',
+          ],
+        ],
+        //#endregion  //*======== Types ===========
+        'disabled:cursor-not-allowed',
+        className,
+      )}
+    >
+      {LeftIcon && (
+        <div className={clsxMerge([size === 'base' && 'mr-2'])}>
+          <LeftIcon
+            className={clsxMerge(
+              [size === 'base' && 'h-5 w-5'],
+              leftIconClassName,
+            )}
+          />
+        </div>
+      )}
+      {children}
+      {RightIcon && (
+        <div className={clsxMerge([size === 'base' && 'ml-2'])}>
+          <RightIcon
+            className={clsxMerge(
+              [size === 'base' && 'h-5 w-5'],
+              rightIconClassName,
+            )}
+          />
+        </div>
+      )}
+    </Link>
+  );
+};
 
 export default ButtonLink;
--- src/components/links/IconLink.tsx
+++ src/components/links/IconLink.tsx
@@ -1,11 +1,7 @@
-import { forwardRef } from 'react';
+import Link from 'next/link';
 import { IconType } from 'react-icons';
 
 import clsxMerge from '@/lib/clsxMerge';
-
-import UnstyledLink, {
-  UnstyledLinkProps,
-} from '@/components/links/UnstyledLink';
 
 enum IconLinkType {
   'primary',
@@ -14,52 +10,55 @@ enum IconLinkType {
 }
 
 type IconLinkProps = {
+  className?: string;
+  href: string;
   type?: keyof typeof IconLinkType;
   icon?: IconType;
   label: string;
   iconClassName?: string;
-} & Omit<UnstyledLinkProps, 'children'>;
+};
 
-const IconLink = forwardRef<HTMLAnchorElement, IconLinkProps>(
-  (
-    { className, icon: Icon, label, type = 'text', iconClassName, ...rest },
-    ref,
-  ) => {
-    return (
-      <UnstyledLink
-        aria-label={label}
-        ref={ref}
-        type='button'
-        className={clsxMerge(
-          'inline-flex items-center justify-center rounded-full text-2xl font-medium',
-          'min-h-[28px] min-w-[28px] p-2.5 md:min-h-[34px] md:min-w-[34px]',
-          [
-            type === 'primary' && [
-              'bg-primary-500 text-white',
-              'border-primary-600 border',
-              'hover:bg-primary-600 hover:text-white',
-              'active:bg-primary-700',
-              'disabled:bg-primary-700',
-            ],
-            type === 'outline' && [
-              'text-primary-500',
-              'border-primary-500 border',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-            ],
-            type === 'text' && [
-              'hover:bg-primary-50 dark:hover:bg-primary-400/10',
-              'hover:text-primary-600 dark:hover:text-primary-400',
-            ],
+const IconLink = ({
+  className,
+  icon: Icon,
+  label,
+  type = 'text',
+  iconClassName,
+  ...rest
+}: IconLinkProps) => {
+  return (
+    <Link
+      aria-label={label}
+      type='button'
+      className={clsxMerge(
+        'inline-flex items-center justify-center rounded-full text-2xl font-medium',
+        'min-h-[28px] min-w-[28px] p-2.5 md:min-h-[34px] md:min-w-[34px]',
+        [
+          type === 'primary' && [
+            'bg-primary-500 text-white',
+            'border-primary-600 border',
+            'hover:bg-primary-600 hover:text-white',
+            'active:bg-primary-700',
+            'disabled:bg-primary-700',
           ],
-          'disabled:cursor-not-allowed',
-          className,
-        )}
-        {...rest}
-      >
-        {Icon && <Icon className={clsxMerge(iconClassName)} />}
-      </UnstyledLink>
-    );
-  },
-);
+          type === 'outline' && [
+            'text-primary-500',
+            'border-primary-500 border',
+            'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
+          ],
+          type === 'text' && [
+            'hover:bg-primary-50 dark:hover:bg-primary-400/10',
+            'hover:text-primary-600 dark:hover:text-primary-400',
+          ],
+        ],
+        'disabled:cursor-not-allowed',
+        className,
+      )}
+      {...rest}
+    >
+      {Icon && <Icon className={clsxMerge(iconClassName)} />}
+    </Link>
+  );
+};
 
 export default IconLink;
