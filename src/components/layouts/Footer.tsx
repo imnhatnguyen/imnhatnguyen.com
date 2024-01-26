602dcc046f3311d28477b93aec32bae99ff36642
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2024-01-26 17:19:16 +0700

    feat: add behance icon

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,4 +1,4 @@
-import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { SiBehance, SiGithub, SiLinkedin } from 'react-icons/si';
 
 import { EmailButton } from '@/components/buttons/EmailButton';
 import IconLink from '@/components/links/IconLink';
@@ -36,6 +36,11 @@ const socials = [
     href: 'https://linkedin.com/in/masonjnguyen',
     icon: SiLinkedin,
     id: 'Linkedin',
+  },
+  {
+    href: 'https://behance.net/masonjnguyen',
+    icon: SiBehance,
+    id: 'Behance',
   },
   {
     href: 'https://github.com/masonjnguyen',
