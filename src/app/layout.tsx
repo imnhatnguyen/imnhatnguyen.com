68e93c084bf863f95fb12bbc304db1e4caed1e79
masonjnguyen <102733388+masonjnguyen@users.noreply.github.com>
2024-05-22 10:56:03 +0700

    feat: add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 
@@ -54,6 +55,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <GoogleAnalytics gaId={process.env.GA_ID ?? ''} />
       <body className='bg-white dark:bg-dark'>
         <Navbar />
         {children}
